bf449c61ee3f91547c3b9f51fd065d61b9310044
jv88899 <jv88899@gmail.com>
2020-01-05 11:07:18 -0600

    set up navbar component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
 import styles from './header.module.css'
 
 const Header = () => (
     <header
         className={styles.containerFull}
     >
+        <Navbar />
     </header>
 )
 
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './navbar.module.css'
+
+const Navbar = () => (
+    <nav
+        className={styles.container}
+    >
+        From the nav component
+    </nav>
+)
+
+export default Navbar
