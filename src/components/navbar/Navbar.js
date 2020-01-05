978273171d74794d500d29ef9da940cd60a21bc1
jv88899 <jv88899@gmail.com>
2020-01-05 11:09:36 -0600

    created basic navbar structure

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,10 +2,9 @@ import React from 'react'
 import styles from './navbar.module.css'
 
 const Navbar = () => (
-    <nav
-        className={styles.container}
-    >
-        From the nav component
+    <nav className={styles.container}>
+        <div className="branding"></div>
+        <div className="nav-menu"></div>
     </nav>
 )
 
