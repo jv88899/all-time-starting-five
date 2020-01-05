1262f21828deb37b6085d9c994d762686e2f57ef
jv88899 <jv88899@gmail.com>
2020-01-05 10:57:01 -0600

    set up Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => (
+    <header
+        className={styles.containerFull}
+    >
+    </header>
+)
+
+export default Header
